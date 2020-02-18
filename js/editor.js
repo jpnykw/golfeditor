7918e8222b51c68a5c15b3f04991699ee199b2b5
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-02-18 19:32:47 +0900

    :recycle: Up-to-date

--- js/editor.js
+++ js/editor.js
@@ -1,12 +1,10 @@
 let editor = null;
 
 (() => {
-    const keyBuffer = [];
-
     window.onload = () => {
         editor = ace.edit('editor');
         editor.$blockScrolling = Infinity;
-        editor.setFontSize(15);
+        editor.setFontSize(25);
         editor.getSession().setMode("ace/mode/typescript");
         editor.setTheme('ace/theme/tomorrow_night_eighties');
         editor.setValue('// write your nice golfed-code in here.');
@@ -14,23 +12,6 @@ let editor = null;
             enableBasicAutocompletion: true,
             enableSnippets: true,
             enableLiveAutocompletion: true
-        });
-
-        const editorElement = document.getElementById('editor');
-
-        editorElement.addEventListener('keydown', (event) => {
-            keyBuffer[event.keyCode] = true;
-
-            if (keyBuffer[13] && (keyBuffer[17] || keyBuffer[91])) {
-                keyBuffer[13] = false;
-                keyBuffer[17] = false;
-                keyBuffer[91] = false;
-                render(editor.getValue());
-            }
-        });
-
-        editorElement.addEventListener('keyup', (event) => {
-            keyBuffer[event.keyCode] = false;
         });
     };
 })();
